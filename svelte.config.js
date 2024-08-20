93da6ac6db593f813bf7ec5c241abb3ada9099cd
Xayanide <xayanide@gmail.com>
2024-08-21 06:44:01 +0800

    chore: change adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 export default {
 	preprocess: vitePreprocess(),
@@ -8,12 +8,6 @@ export default {
 			routes: {
 				include: ['/*'],
 				exclude: ['<all>']
-			},
-			platformProxy: {
-				configPath: 'wrangler.toml',
-				environment: undefined,
-				experimentalJsonConfig: false,
-				persist: false
 			}
 		})
 	}
